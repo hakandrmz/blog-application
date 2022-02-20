db613dbd5216927b4f96a4345135418622a4d443
Hakan Durmaz <durmazhakan@icloud.com>
2022-02-20 14:06:31 +0300

    update amqp and notification servicde

--- notification/src/main/java/guru/hakandurmaz/notification/NotificationApplication.java
+++ notification/src/main/java/guru/hakandurmaz/notification/NotificationApplication.java
@@ -1,11 +1,32 @@
 package guru.hakandurmaz.notification;
 
+import guru.hakandurmaz.amqp.RabbitMQMessageProducer;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {"guru.hakandurmaz.amqp",
+                            "guru.hakandurmaz.notification"
+        }
+)
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
     }
+
+    @Bean
+    CommandLineRunner commandLineRunner(RabbitMQMessageProducer producer,
+                                        NotificationConfig notificationConfig) {
+        return args -> {
+            producer.publish(new Person("Hakan",2),
+                    notificationConfig.getInternalExchange(),
+                    notificationConfig.getInternalNotificationRoutingKey());
+        };
+
+
+    }
+
+    record Person(String name,int age) {}
 }
