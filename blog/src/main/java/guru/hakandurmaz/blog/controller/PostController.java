4bddda10161c2de7b6112e61c90ad4c0a32608d9
hakandurmaz <durmazhakan@icloud.com>
2023-07-29 17:14:18 +0300

    fixed docker compose file

--- blog/src/main/java/guru/hakandurmaz/blog/controller/PostController.java
+++ blog/src/main/java/guru/hakandurmaz/blog/controller/PostController.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping("/posts")
+@RequestMapping("api/posts")
 public class PostController {
 
   private final PostService postService;
